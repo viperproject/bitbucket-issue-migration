ff83698c408797de4b001a6b206e001e29717b7f
Federico Poli <federpoli@gmail.com>
2020-01-24 09:42:01 +0100

    Add missing files

---
+++ migrate/vcs.py
@@ -0,0 +1,9 @@
+class VcsMigration:
+    def __init__(self):
+        pass
+
+    def hg_commit_to_git(self, commit_hash):
+        return None
+
+    def hg_branch_to_git(self, branch_name):
+        return None
