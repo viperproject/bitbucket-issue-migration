8ac85eeb883e839cc1fc913c463e58444668505d
Federico Poli <federpoli@gmail.com>
2020-01-24 09:47:02 +0100

    Remove unused file

--- migrate/vcs.py
+++
@@ -1,9 +0,0 @@
-class VcsMigration:
-    def __init__(self):
-        pass
-
-    def hg_commit_to_git(self, commit_hash):
-        return None
-
-    def hg_branch_to_git(self, branch_name):
-        return None
